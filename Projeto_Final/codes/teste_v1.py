a4a7621a0dd61cecbb255be045d5e78c918c4464
Douglas Queiroz <douglas_doug5040@hotmail.com>
2021-07-13 00:43:48 -0300

    Implemntando metodo Otsu thresholding

--- Projeto_Final/codes/teste_v1.py
+++ Projeto_Final/codes/teste_v1.py
@@ -37,12 +37,17 @@ import time
 # camera.stop_preview()
 
 
-for file in glob.glob("imgs/*"):
+for file in glob.glob("../imgs/in/*"):
   img = cv2.imread(file)
   h,w = img.shape[0:2]
-  cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+  blur_hor = cv2.filter2D(img[:, :, 0], cv2.CV_32F, kernel=np.ones((11,1,1), np.float32)/11.0, borderType=cv2.BORDER_CONSTANT)
+  blur_vert = cv2.filter2D(img[:, :, 0], cv2.CV_32F, kernel=np.ones((1,11,1), np.float32)/11.0, borderType=cv2.BORDER_CONSTANT)
+  mask = ((img[:,:,0]>blur_hor*1.2) | (img[:,:,0]>blur_vert*1.2)).astype(np.uint8)*255
   
-  ret, limites = cv2.threshold (cinza, 127, 255, 0)
+  #cinza = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+
+  ret, limites = cv2.threshold (mask, 127, 255, 0)
   bordas = cv2.Canny(img, 50, 200, None, 3)
   
   contours, hierarchy = cv2.findContours(limites, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
